2e717f7566a7d187127c0aab154ebb8f231b7c85
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2022-10-16 11:33:22 +0200

    installed CORS for cross reference resource sharing between browser and server

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+//data
 let persons = [
     { 
       "id": 1,
@@ -24,15 +27,57 @@ let persons = [
     }
 ]
 
-
+//homepage of our api server
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
+//info page for api requests
+app.get('/info', (request, response) => {
+  const count  = `There are currently ${persons.length} contacts in the database` 
+  const date =  new Date()
+  const info = `${count} as at ${date}`
+  response.send(info)
+})
+
+//get a single person resource endpoint
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  //making sure the right status code appears when a wrong index number is called
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+//get all persons in database
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+//delete a resource node
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  
+  response.status(204).end()
+})
+
+//add an entry to the person list
+app.post('/api/persons', (request, response) => {
+  const maxId = persons.length > 0
+  ? Math.max(...persons.map(n => n.id)) 
+  : 0
+
+  const person = request.body
+  person.id = maxId + 1
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+//specify the port for the server
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
